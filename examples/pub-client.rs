d8e1ffe6f88b363383a967d43f61c9b1cbbdb1d8
Y. T. Chung <zonyitoo@gmail.com>
2018-09-27 00:16:20 +0800

    Make example built with new dependencies

--- examples/pub-client.rs
+++ examples/pub-client.rs
@@ -1,8 +1,8 @@
 extern crate mqtt;
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 extern crate clap;
+extern crate env_logger;
 extern crate uuid;
 
 use std::env;
@@ -14,64 +14,67 @@ use clap::{App, Arg};
 
 use uuid::Uuid;
 
-use mqtt::{Decodable, Encodable, QualityOfService};
-use mqtt::{TopicFilter, TopicName};
 use mqtt::control::variable_header::ConnectReturnCode;
 use mqtt::packet::*;
+use mqtt::{Decodable, Encodable, QualityOfService};
+use mqtt::{TopicFilter, TopicName};
 
 fn generate_client_id() -> String {
-    format!("/MQTT/rust/{}", Uuid::new_v4().simple().to_string())
+    format!("/MQTT/rust/{}", Uuid::new_v4())
 }
 
 fn main() {
     // configure logging
-    env::set_var(
-        "RUST_LOG",
-        env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()),
-    );
-    env_logger::init().unwrap();
+    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
+    env_logger::init();
 
     let matches = App::new("sub-client")
         .author("Y. T. Chung <zonyitoo@gmail.com>")
-        .arg(Arg::with_name("SERVER")
-                 .short("S")
-                 .long("server")
-                 .takes_value(true)
-                 .required(true)
-                 .help("MQTT server address (host:port)"))
-        .arg(Arg::with_name("SUBSCRIBE")
-                 .short("s")
-                 .long("subscribe")
-                 .takes_value(true)
-                 .multiple(true)
-                 .required(true)
-                 .help("Channel filter to subscribe"))
-        .arg(Arg::with_name("USER_NAME")
-                 .short("u")
-                 .long("username")
-                 .takes_value(true)
-                 .help("Login user name"))
-        .arg(Arg::with_name("PASSWORD")
-                 .short("p")
-                 .long("password")
-                 .takes_value(true)
-                 .help("Password"))
-        .arg(Arg::with_name("CLIENT_ID")
-                 .short("i")
-                 .long("client-identifier")
-                 .takes_value(true)
-                 .help("Client identifier"))
-        .get_matches();
+        .arg(
+            Arg::with_name("SERVER")
+                .short("S")
+                .long("server")
+                .takes_value(true)
+                .required(true)
+                .help("MQTT server address (host:port)"),
+        ).arg(
+            Arg::with_name("SUBSCRIBE")
+                .short("s")
+                .long("subscribe")
+                .takes_value(true)
+                .multiple(true)
+                .required(true)
+                .help("Channel filter to subscribe"),
+        ).arg(
+            Arg::with_name("USER_NAME")
+                .short("u")
+                .long("username")
+                .takes_value(true)
+                .help("Login user name"),
+        ).arg(
+            Arg::with_name("PASSWORD")
+                .short("p")
+                .long("password")
+                .takes_value(true)
+                .help("Password"),
+        ).arg(
+            Arg::with_name("CLIENT_ID")
+                .short("i")
+                .long("client-identifier")
+                .takes_value(true)
+                .help("Client identifier"),
+        ).get_matches();
 
     let server_addr = matches.value_of("SERVER").unwrap();
-    let client_id = matches.value_of("CLIENT_ID")
-                           .map(|x| x.to_owned())
-                           .unwrap_or_else(generate_client_id);
-    let channel_filters: Vec<(TopicFilter, QualityOfService)> =
-        matches.values_of("SUBSCRIBE")
-               .unwrap()
-               .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
-               .collect();
+    let client_id = matches
+        .value_of("CLIENT_ID")
+        .map(|x| x.to_owned())
+        .unwrap_or_else(generate_client_id);
+    let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
+        .values_of("SUBSCRIBE")
+        .unwrap()
+        .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
+        .collect();
 
     info!("Connecting to {:?} ... ", server_addr);
     let mut stream = TcpStream::connect(server_addr).unwrap();
@@ -88,7 +91,10 @@ fn main() {
     trace!("CONNACK {:?}", connack);
 
     if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
-        panic!("Failed to connect to server, return code {:?}", connack.connect_return_code());
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
     }
 
     info!("Applying channel filters {:?} ...", channel_filters);
@@ -97,10 +103,11 @@ fn main() {
     sub.encode(&mut buf).unwrap();
     stream.write_all(&buf[..]).unwrap();
 
-    let channels: Vec<TopicName> = matches.values_of("SUBSCRIBE")
-                                          .unwrap()
-                                          .map(|c| TopicName::new(c.to_string()).unwrap())
-                                          .collect();
+    let channels: Vec<TopicName> = matches
+        .values_of("SUBSCRIBE")
+        .unwrap()
+        .map(|c| TopicName::new(c.to_string()).unwrap())
+        .collect();
 
     let user_name = matches.value_of("USER_NAME").unwrap_or("<anonym>");
 
--- examples/sub-client-async.rs
+++ examples/sub-client-async.rs
@@ -1,36 +1,36 @@
 extern crate mqtt;
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 extern crate clap;
-extern crate uuid;
+extern crate env_logger;
 extern crate futures;
 extern crate tokio;
+extern crate uuid;
 
 use std::env;
 use std::fmt::Debug;
 use std::io::Write;
-use std::str;
 use std::net;
+use std::str;
 use std::time::{Duration, Instant};
 
 use clap::{App, Arg};
 
 use uuid::Uuid;
 
-use futures::{Future, Stream, future};
+use futures::{future, Future, Stream};
 
-use tokio::net::TcpStream;
 use tokio::io::{self, AsyncRead};
+use tokio::net::TcpStream;
 use tokio::timer::Interval;
 
-use mqtt::{Decodable, Encodable, QualityOfService};
-use mqtt::TopicFilter;
 use mqtt::control::variable_header::ConnectReturnCode;
 use mqtt::packet::*;
+use mqtt::TopicFilter;
+use mqtt::{Decodable, Encodable, QualityOfService};
 
 fn generate_client_id() -> String {
-    format!("/MQTT/rust/{}", Uuid::new_v4().simple())
+    format!("/MQTT/rust/{}", Uuid::new_v4())
 }
 
 fn alt_drop<E: Debug>(err: E) {
@@ -39,53 +39,56 @@ fn alt_drop<E: Debug>(err: E) {
 
 fn main() {
     // configure logging
-    env::set_var(
-        "RUST_LOG",
-        env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()),
-    );
-    env_logger::init().unwrap();
+    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
+    env_logger::init();
 
     let matches = App::new("sub-client")
         .author("Y. T. Chung <zonyitoo@gmail.com>")
-        .arg(Arg::with_name("SERVER")
-                 .short("S")
-                 .long("server")
-                 .takes_value(true)
-                 .required(true)
-                 .help("MQTT server address (host:port)"))
-        .arg(Arg::with_name("SUBSCRIBE")
-                 .short("s")
-                 .long("subscribe")
-                 .takes_value(true)
-                 .multiple(true)
-                 .required(true)
-                 .help("Channel filter to subscribe"))
-        .arg(Arg::with_name("USER_NAME")
-                 .short("u")
-                 .long("username")
-                 .takes_value(true)
-                 .help("Login user name"))
-        .arg(Arg::with_name("PASSWORD")
-                 .short("p")
-                 .long("password")
-                 .takes_value(true)
-                 .help("Password"))
-        .arg(Arg::with_name("CLIENT_ID")
-                 .short("i")
-                 .long("client-identifier")
-                 .takes_value(true)
-                 .help("Client identifier"))
-        .get_matches();
+        .arg(
+            Arg::with_name("SERVER")
+                .short("S")
+                .long("server")
+                .takes_value(true)
+                .required(true)
+                .help("MQTT server address (host:port)"),
+        ).arg(
+            Arg::with_name("SUBSCRIBE")
+                .short("s")
+                .long("subscribe")
+                .takes_value(true)
+                .multiple(true)
+                .required(true)
+                .help("Channel filter to subscribe"),
+        ).arg(
+            Arg::with_name("USER_NAME")
+                .short("u")
+                .long("username")
+                .takes_value(true)
+                .help("Login user name"),
+        ).arg(
+            Arg::with_name("PASSWORD")
+                .short("p")
+                .long("password")
+                .takes_value(true)
+                .help("Password"),
+        ).arg(
+            Arg::with_name("CLIENT_ID")
+                .short("i")
+                .long("client-identifier")
+                .takes_value(true)
+                .help("Client identifier"),
+        ).get_matches();
 
     let server_addr = matches.value_of("SERVER").unwrap();
-    let client_id = matches.value_of("CLIENT_ID")
-                           .map(|x| x.to_owned())
-                           .unwrap_or_else(generate_client_id);
-    let channel_filters: Vec<(TopicFilter, QualityOfService)> =
-        matches.values_of("SUBSCRIBE")
-               .unwrap()
-               .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
-               .collect();
+    let client_id = matches
+        .value_of("CLIENT_ID")
+        .map(|x| x.to_owned())
+        .unwrap_or_else(generate_client_id);
+    let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
+        .values_of("SUBSCRIBE")
+        .unwrap()
+        .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
+        .collect();
 
     let keep_alive = 10;
 
@@ -105,7 +108,10 @@ fn main() {
     trace!("CONNACK {:?}", connack);
 
     if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
-        panic!("Failed to connect to server, return code {:?}", connack.connect_return_code());
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
     }
 
     // const CHANNEL_FILTER: &'static str = "typing-speed-test.aoeu.eu";
@@ -136,52 +142,52 @@ fn main() {
     }
 
     // connection made, start the async work
-    let program = future::ok(())
-        .and_then(move |()| {
-            let stream = TcpStream::from_std(stream, &Default::default()).unwrap();
-            let (mqtt_read, mqtt_write) = stream.split();
+    let program = future::ok(()).and_then(move |()| {
+        let stream = TcpStream::from_std(stream, &Default::default()).unwrap();
+        let (mqtt_read, mqtt_write) = stream.split();
 
-            let ping_time = Duration::new((keep_alive / 2) as u64, 0);
-            let ping_stream = Interval::new(Instant::now() + ping_time, ping_time);
+        let ping_time = Duration::new((keep_alive / 2) as u64, 0);
+        let ping_stream = Interval::new(Instant::now() + ping_time, ping_time);
 
-            let ping_sender = ping_stream.map_err(alt_drop).fold(mqtt_write, |mqtt_write, _| {
-                info!("Sending PINGREQ to broker");
+        let ping_sender = ping_stream.map_err(alt_drop).fold(mqtt_write, |mqtt_write, _| {
+            info!("Sending PINGREQ to broker");
 
-                let pingreq_packet = PingreqPacket::new();
+            let pingreq_packet = PingreqPacket::new();
 
-                let mut buf = Vec::new();
-                pingreq_packet.encode(&mut buf).unwrap();
-                io::write_all(mqtt_write, buf).map(|(mqtt_write, _buf)| mqtt_write).map_err(alt_drop)
-            });
-
-            let receiver = future::loop_fn::<_, (), _, _>(mqtt_read, |mqtt_read| {
-                VariablePacket::parse(mqtt_read)
-                    .map(|(mqtt_read, packet)| {
-                        trace!("PACKET {:?}", packet);
-
-                        match packet {
-                            VariablePacket::PingrespPacket(..) => {
-                                info!("Receiving PINGRESP from broker ..");
-                            }
-                            VariablePacket::PublishPacket(ref publ) => {
-                                let msg = match str::from_utf8(&publ.payload_ref()[..]) {
-                                    Ok(msg) => msg,
-                                    Err(err) => {
-                                        error!("Failed to decode publish message {:?}", err);
-                                        return future::Loop::Continue(mqtt_read);
-                                    }
-                                };
-                                info!("PUBLISH ({}): {}", publ.topic_name(), msg);
-                            }
-                            _ => {}
-                        }
-
-                        future::Loop::Continue(mqtt_read)
-                    })
-                }).map_err(alt_drop);
-
-            ping_sender.join(receiver).map(alt_drop)
+            let mut buf = Vec::new();
+            pingreq_packet.encode(&mut buf).unwrap();
+            io::write_all(mqtt_write, buf)
+                .map(|(mqtt_write, _buf)| mqtt_write)
+                .map_err(alt_drop)
         });
+
+        let receiver = future::loop_fn::<_, (), _, _>(mqtt_read, |mqtt_read| {
+            VariablePacket::parse(mqtt_read).map(|(mqtt_read, packet)| {
+                trace!("PACKET {:?}", packet);
+
+                match packet {
+                    VariablePacket::PingrespPacket(..) => {
+                        info!("Receiving PINGRESP from broker ..");
+                    }
+                    VariablePacket::PublishPacket(ref publ) => {
+                        let msg = match str::from_utf8(&publ.payload_ref()[..]) {
+                            Ok(msg) => msg,
+                            Err(err) => {
+                                error!("Failed to decode publish message {:?}", err);
+                                return future::Loop::Continue(mqtt_read);
+                            }
+                        };
+                        info!("PUBLISH ({}): {}", publ.topic_name(), msg);
+                    }
+                    _ => {}
+                }
+
+                future::Loop::Continue(mqtt_read)
+            })
+        }).map_err(alt_drop);
+
+        ping_sender.join(receiver).map(alt_drop)
+    });
 
     tokio::run(program);
 }

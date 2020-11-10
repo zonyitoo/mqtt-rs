df6350b76ba25cd1942fffec93ffa3a9871440d7
Jeff Hiner <jhiner@dwelo.com>
2020-11-10 10:55:16 -0700

    Add getters for protocol version and name; default to "MQTT" for name

--- examples/pub-client.rs
+++ examples/pub-client.rs
@@ -81,7 +81,7 @@ fn main() {
     info!("Connected!");
 
     info!("Client identifier {:?}", client_id);
-    let mut conn = ConnectPacket::new("MQTT", client_id);
+    let mut conn = ConnectPacket::new(client_id);
     conn.set_clean_session(true);
     let mut buf = Vec::new();
     conn.encode(&mut buf).unwrap();
